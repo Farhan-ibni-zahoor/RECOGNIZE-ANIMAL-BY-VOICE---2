2f1fb2608d81456087f000d2741ae2f6aa0a0c22
Farhan-ibni-zahoor <87404631+Farhan-ibni-zahoor@users.noreply.github.com>
2022-03-21 16:38:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function startClassifictaion() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/PdPXAG9u6/',modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResult);
+}
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
